7ddff1a3246dfc462720a5b7c8d7bad0f1c07704
unknown <jnogra@stackify.com>
2018-11-14 15:40:46 +0800

    Add user.

---
+++ add-contact.php
@@ -0,0 +1,16 @@
+<?php
+
+	require_once('db.php');
+
+	$sql = 'INSERT INTO contacts (key, name, address, photo) VALUES ("'.bin2hex(random_bytes(8)).'", "'.$_POST['name'].'", "'.$_POST['address'].'", "'.$_POST['photo'].'", )';
+	$result = $conn->query($sql);
+
+	$rows = array(
+		'status'		=> 1,
+		'mesage' 		=> 'User has been added',
+	);
+	print json_encode($rows);
+
+	$conn->close();
+
+?>
