7168cd474891b29751c89f31db898ce1f730af2a
unknown <jnogra@stackify.com>
2018-11-14 15:56:44 +0800

    Add user.

--- add-contact.php
+++ add-contact.php
@@ -3,7 +3,6 @@
 	require_once('db.php');
 
 	$sql = 'INSERT INTO contacts (key, name, address, photo) VALUES ("'.bin2hex(random_bytes(8)).'", "'.$_POST['name'].'", "'.$_POST['address'].'", "'.$_POST['photo'].'")';
-	echo $sql;
 	if ($conn->query($sql) === TRUE) {
 	    $rows = array(
 			'status'		=> 1,
@@ -14,6 +13,7 @@
 			'status'		=> 0,
 			'mesage' 		=> 'Fail in adding contact!',
 			'error'			=> $conn->error,
+			'query'			=> $sql
 		);
 	}
 	
