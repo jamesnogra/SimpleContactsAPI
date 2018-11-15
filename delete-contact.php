04f931abab5982d8d580f3a9e30123a4cd4dcee0
unknown <jnogra@stackify.com>
2018-11-15 10:29:35 +0800

    Added the delete function.

---
+++ delete-contact.php
@@ -0,0 +1,24 @@
+<?php
+
+	require_once('db.php');
+
+	$sql = 'DELETE FROM contacts WHERE id="'.$_POST['id'].'")';
+	if ($conn->query($sql) === TRUE) {
+	    $rows = array(
+			'status'		=> 1,
+			'mesage' 		=> 'Contact has been deleted',
+		);
+	} else {
+	    $rows = array(
+			'status'		=> 0,
+			'mesage' 		=> 'Fail in deleting contact!',
+			'error'			=> $conn->error,
+			'query'			=> $sql
+		);
+	}
+	
+	print json_encode($rows);
+
+	$conn->close();
+
+?>
